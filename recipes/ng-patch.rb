507959722cf9b9370718614f5209d75f8248648b
Chaman <chaman.kang@kt.com>
2013-05-02 15:18:41 +0900

    modify the ng-patch recipe

--- recipes/ng-patch.rb
+++ recipes/ng-patch.rb
@@ -5,20 +5,15 @@
 include_recipe "osops-utils"
 
 # add /32 subnet support
-template "/usr/share/pyshared/quantum/db/db_base_plugin_v2.py" do
-  source "ktc-patches/db_base_plugin_v2.py.2012.2.1-0ubuntu1~cloud0"
-  owner "root"
-  owner "root"
-  mode "0644"
-  notifies :restart, resources(:service => "quantum-server"), :immediately
-  only_if { ::Chef::Recipe::Patch.check_package_version("quantum-server","2012.2.1-0ubuntu1~cloud0",node) }
+%w{ 2012.2.1-0ubuntu1~cloud0 2012.2.3-0ubuntu2~cloud0 }.each do |version|
+  if ::Chef::Recipe::Patch.check_package_version("quantum-server",version,node)
+    template "/usr/share/pyshared/quantum/db/db_base_plugin_v2.py" do
+      source "ktc-patches/db_base_plugin_v2.py.#{version}"
+      owner "root"
+      owner "root"
+      mode "0644"
+      notifies :restart, resources(:service => "quantum-server"), :immediately
+    end
+  end
 end
 
-template "/usr/share/pyshared/quantum/db/db_base_plugin_v2.py" do
-  source "ktc-patches/db_base_plugin_v2.py.2012.2.3-0ubuntu2~cloud0"
-  owner "root"
-  owner "root"
-  mode "0644"
-  notifies :restart, resources(:service => "quantum-server"), :immediately
-  only_if { ::Chef::Recipe::Patch.check_package_version("quantum-server","2012.2.3-0ubuntu2~cloud0",node) }
-end
