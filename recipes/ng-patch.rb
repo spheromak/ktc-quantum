f44b93e660265e30b94dbbcfdde22e015176a9bc
Chaman <chaman.kang@kt.com>
2013-05-02 14:51:51 +0900

    add a new quantum-server patch in the ng-patch recipe

--- recipes/ng-patch.rb
+++ recipes/ng-patch.rb
@@ -13,3 +13,12 @@ template "/usr/share/pyshared/quantum/db/db_base_plugin_v2.py" do
   notifies :restart, resources(:service => "quantum-server"), :immediately
   only_if { ::Chef::Recipe::Patch.check_package_version("quantum-server","2012.2.1-0ubuntu1~cloud0",node) }
 end
+
+template "/usr/share/pyshared/quantum/db/db_base_plugin_v2.py" do
+  source "ktc-patches/db_base_plugin_v2.py.2012.2.3-0ubuntu2~cloud0"
+  owner "root"
+  owner "root"
+  mode "0644"
+  notifies :restart, resources(:service => "quantum-server"), :immediately
+  only_if { ::Chef::Recipe::Patch.check_package_version("quantum-server","2012.2.3-0ubuntu2~cloud0",node) }
+end
