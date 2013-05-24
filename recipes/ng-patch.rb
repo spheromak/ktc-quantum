dd56ce81d704239cdf66b7cb1861dc5a6367a327
Chaman <chaman.kang@kt.com>
2013-05-24 12:11:49 +0900

    UCLOUDNG-277 Create recipe to patch quantum bug 1171997

--- recipes/ng-patch.rb
+++ recipes/ng-patch.rb
@@ -4,12 +4,23 @@
 
 include_recipe "osops-utils"
 
-# add /32 subnet support
-template "/usr/share/pyshared/quantum/db/db_base_plugin_v2.py" do
-  source "ktc-patches/db_base_plugin_v2.py.python-quantum"
-  owner "root"
-  owner "root"
-  mode "0644"
-  notifies :restart, resources(:service => "quantum-server"), :immediately
-  only_if { ::Chef::Recipe::Patch.check_package_version("quantum-server","1:2013.1-0ubuntu2~cloud0",node) }
+%w{ 1:2013.1-0ubuntu2~cloud0}.each do |version|
+  if ::Chef::Recipe::Patch.check_package_version("nova-compute",version,node)
+    # add /32 subnet support
+    template "/usr/share/pyshared/quantum/db/db_base_plugin_v2.py" do
+      source "ktc-patches/db_base_plugin_v2.py.#{version}"
+      owner "root"
+      owner "root"
+      mode "0644"
+      notifies :restart, resources(:service => "quantum-server"), :immediately
+    end
+
+    template "/usr/share/pyshared/quantum/db/securitygroups_db.py" do
+      source "ktc-patches/securitygroups_db.py.#{version}"
+      owner "root"
+      owner "root"
+      mode "0644"
+      notifies :restart, resources(:service => "quantum-server"), :immediately
+    end
+  end
 end
